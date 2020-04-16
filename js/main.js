f3193227ceca3964c93690fb4a33e6013b8f9700
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-04-16 12:49:07 +0300

    main.js changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 	$(".rate .item").click(function() {
+	$('#plan-select-form [name="plan"]').val(3 - $(this).index());
 	$(".rate .item").removeClass("item--green").eq($(this).index()).addClass("item--green");
 }).eq(0).addClass("active");
-
 });
